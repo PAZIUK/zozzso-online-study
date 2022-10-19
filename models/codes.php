87237e32d9c2622d8ce39e05e2034e8ce5d5e0a3
serhiy <paz.serhiy@gmail.com>
2022-10-19 20:08:58 +0300

    Add some changes into rules page and isCodeBelongsAdmin function

--- models/codes.php
+++ models/codes.php
@@ -19,15 +19,11 @@
 		}
 		public static function isCodeBelongsAdmin($code){
 			$mysqli = DATABASE::Connect();
-			$sql = "SELECT * FROM `zozzso-online-study_codes` WHERE `Codes_Code`= ? ";
+			$sql = "SELECT `Roles_Role` FROM `zozzso-online-study_roles` INNER JOIN `zozzso-online-study_codes` ON `zozzso-online-study_codes`.`Codes_ID` = `zozzso-online-study_roles`.`Roles_CodeID` WHERE `zozzso-online-study_codes`.`Codes_Code`= ? ";
 			$stmt = $mysqli->prepare($sql);
       $stmt->bind_param("i", $code);
 			$stmt->execute();
-			// return count($stmt->get_result()->fetch_all(MYSQLI_ASSOC))!=0;
-			if($code == "71089"){
-				return true;
-			}
-			return false;
+			return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]["Roles_Role"] == "Admin";
 		}
 	}
 ?>
